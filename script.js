62adfc2397e9556d64d4f98faaae3e5fc4bfa321
olubiyi09 <olubiyi09@gmail.com>
2022-09-14 19:29:22 +0400

    added more function to the app

--- script.js
+++ script.js
@@ -6,12 +6,24 @@ button.addEventListener("click", copyInput);
 function copyInput(e) {
   e.preventDefault();
 
-  inputField.select();
-  inputField.setSelectionRange(0, 1000);
-  document.execCommand("copy");
-  button.innerHTML = `Copied!!!`;
+  // inputField.select();
+  // inputField.setSelectionRange(0, 1000);
+  // document.execCommand("copy");
+  if (inputField.value == "") {
+    alert("Nothing to copy!!!");
+  }
+  navigator.clipboard.writeText(inputField.value).then(() => {
+    inputField.select();
+    button.innerHTML = `Copied!!!`;
 
-  setTimeout(() => {
-    button.innerHTML = `Copy`;
-  }, 2000);
+    setTimeout(() => {
+      button.innerHTML = `Copy`;
+    }, 2000);
+  });
+
+  // button.innerHTML = `Copied!!!`;
+
+  // setTimeout(() => {
+  //   button.innerHTML = `Copy`;
+  // }, 2000);
 }
